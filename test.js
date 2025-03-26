babe0a2ef1226cbb1ba1f5155ebcd573b3c8410b
Ahmed elenany <elenanyahmed30@gmail.com>
2025-03-26 23:21:58 +0000

    Added Test Case

---
+++ test.js
@@ -0,0 +1,12 @@
+// requrire a .env file in the root directory of the application
+require("dotenv").config(); 
+
+describe("Environment Variables Check", () => {
+  test("should have all required environment variables", () => {
+    expect(process.env.DB_HOST).toBeTruthy();
+    expect(process.env.USER).toBeTruthy();
+    expect(process.env.DB_PASS).toBeTruthy();
+    expect(process.env.DB_NAME).toBeTruthy();
+  });
+});
+
