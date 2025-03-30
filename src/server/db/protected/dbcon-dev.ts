0fdc42678e135f03ead483994e54ed58a3384f68
Ahmed elenany <elenanyahmed30@gmail.com>
2025-03-30 10:23:37 +0000

    Edit Src

--- src/server/db/protected/dbcon-dev.ts
+++ src/server/db/protected/dbcon-dev.ts
@@ -4,7 +4,7 @@ export const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user:  process.env.USER,
-    password: process.env.DB_PASS,
+    password: process.env.USER_PASS,
     database: process.env.DB_NAME,
     port: 3306,
     connectTimeout: 10000
